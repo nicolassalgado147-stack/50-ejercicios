859d4fed9dd8b9be8051654b6af45273ec620199
nicolassalgado147-stack <nicolassalgado147@gmail.com>
2025-09-26 10:36:32 -0500

    Add files via upload

---
+++ listas1/listas4.py
@@ -0,0 +1,12 @@
+# Nicolas Salgado
+class Suma:
+    def __init__(self, lista):
+        self.lista = lista
+
+    def suma2(self):
+        print("Lista:", self.lista)
+        print("Suma:", sum(self.lista))
+
+
+obj4 = Suma([10, 20, 35, 60])
+obj4.suma2()

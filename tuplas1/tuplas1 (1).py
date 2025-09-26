2368281c5ecfc60024447b43e97d070915e463fd
nicolassalgado147-stack <nicolassalgado147@gmail.com>
2025-09-26 10:37:03 -0500

    Add files via upload

---
+++ tuplas1/tuplas1 (1).py
@@ -0,0 +1,12 @@
+# Nicolas Salgado
+class tupla1:
+    def __init__(self):
+        self.tupla = ("python", "java", "c++", "django")
+
+    def ver(self):
+        print("Primero:", self.tupla[0])
+        print("Último:", self.tupla[-1])
+
+
+obj1 = tupla1()
+obj1.ver()
---
+++ tuplas1/tuplas3 (1).py
@@ -0,0 +1,11 @@
+# Nicolas Salgado
+class tupla3:
+    def __init__(self):
+        self.tupla = (1, 2, 2, 3, 4, 2, 5)
+
+    def c(self, n):
+        print(f"El número {n} aparece {self.tupla.count(n)}veces")
+
+
+obj3 = tupla3()
+obj3.c(2)

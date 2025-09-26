9635544e16dc2aad9c8f10433790a04b6381741e
nicolassalgado147-stack <nicolassalgado147@gmail.com>
2025-09-26 10:35:39 -0500

    Add files via upload

---
+++ ejercicios 1/4.py
@@ -0,0 +1,11 @@
+# Nicolas Salgado
+def actualizar(dic, nombre, nueva):
+    if nombre in dic:
+        edad = dic[nombre][0]
+        dic[nombre] = (edad, nueva)
+        return True
+    return False
+
+if __name__ == "__main__":
+    d = {'Ana':(30,'B')}
+    print(actualizar(d,'larita','suame'), d)
